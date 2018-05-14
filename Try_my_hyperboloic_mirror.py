7602087794d17c4918e71b326ed1395ae086b338
Yiones Aouadi <yaouadi@gurb.esrf.fr>
2018-05-14 16:08:25 +0200

    hyperbola -> make it focus

--- Try_my_hyperboloic_mirror.py
+++ Try_my_hyperboloic_mirror.py
@@ -6,39 +6,109 @@ import matplotlib.pyplot as plt
 from numpy.testing import assert_almost_equal
 
 
-beam=Beam(1)
-#beam.set_flat_divergence(0.000005,0.001)
-beam.set_point(0,0,20+np.sqrt(2))
-
-p=20.
-q=np.sqrt(2)
-theta=0*np.pi/180
-
-hyp_mirror=Optical_element.initialize_my_hyperboloid(p,q,theta)
-#ideal_lens=Optical_element()
-#ideal_lens.set_parameters(10.+np.sqrt(2),10.+np.sqrt(2))
-
-
-beam.plot_xz()
-#print(beam.x,beam.vx)
-#beam=ideal_lens.trace_ideal_lens(beam)
-#print(beam.x,beam.vx)
+#beam=Beam(1)
+##beam.set_flat_divergence(0.000005,0.001)
+#beam.set_point(15.,0,20.)
 #
+#beam.plot_xz()
+#
+#
+#
+#p=20.+np.sqrt(2)
+#q=np.sqrt(2)
+#theta=0*np.pi/180
+#
+#hyp_mirror=Optical_element.initialize_my_hyperboloid(p,q,theta)
+#
+#
+#beam.plot_xz()
+#
+#beam=hyp_mirror.trace_optical_element(beam)
+#
+#
+#
+#beam.plot_xz()
+##print (hyp_mirror.ccc_object.get_coefficients())
+#
+#print(beam.x,beam.y,beam.z)
+#
+#plt.show()
 
-beam=hyp_mirror.trace_optical_element(beam)
+beam=Beam()
+beam.set_flat_divergence(0.005,0.0005)
+p=20.
+q=0.
 
 
-#t = (20.+np.sqrt(2)) / beam.vy
+spherical_mirror=Optical_element.initialize_as_spherical_mirror(p,q,theta=0,alpha=0,R=20)
+
+beam=spherical_mirror.trace_optical_element(beam)
+
+#t=20/beam.vy
 #beam.x = beam.x + beam.vx * t
 #beam.y = beam.y + beam.vy * t
 #beam.z = beam.z + beam.vz * t
 
-
+p=20.-np.sqrt(2)
+q=np.sqrt(2)
+theta=0*np.pi/180
+hyp_mirror=Optical_element.initialize_my_hyperboloid(p,q,theta)
 beam.plot_xz()
-print (hyp_mirror.ccc_object.get_coefficients())
-
+beam=hyp_mirror.trace_optical_element(beam)
+#print (hyp_mirror.ccc_object.get_coefficients())
 print(beam.x,beam.y,beam.z)
 
-#beam.histogram()
+beam.plot_xz()
 
 plt.show()
+
+
+
+
+
+
+
+
+
+###
+###
+###beam=Beam(1)
+####beam.set_flat_divergence(0.000005,0.001)
+###beam.set_point(0,0,20)
+###
+###beam.plot_xz()
+###
+###
+###
+###p=20.+np.sqrt(2)
+###q=np.sqrt(2)
+###theta=0*np.pi/180
+###
+###hyp_mirror=Optical_element.initialize_my_hyperboloid(p,q,theta)
+####ideal_lens=Optical_element()
+####ideal_lens.set_parameters(10.+np.sqrt(2),10.+np.sqrt(2))
+###
+###
+###beam.plot_xz()
+####print(beam.x,beam.vx)
+####beam=ideal_lens.trace_ideal_lens(beam)
+####print(beam.x,beam.vx)
+####
+###
+###beam=hyp_mirror.trace_optical_element(beam)
+###
+###
+####t = (20.+np.sqrt(2)) / beam.vy
+####beam.x = beam.x + beam.vx * t
+####beam.y = beam.y + beam.vy * t
+####beam.z = beam.z + beam.vz * t
+###
+###
+###beam.plot_xz()
+####print (hyp_mirror.ccc_object.get_coefficients())
+###
+###print(beam.x,beam.y,beam.z)
+###
+####beam.histogram()
+###
+###plt.show()
